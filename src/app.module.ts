97b3c3e4cdf00ee05d6ed2226aea227a9e4c6625
asmeyb <asmeyb@gmail.com>
2025-11-14 22:32:44 +0300

    Add transactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module, Post } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
-import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 import * as joi from 'joi';
 import appConfig from './config/app.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({  
   imports: [
@@ -30,7 +32,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
           load: [appConfig],         
       })
       }),
-    HelloModule, UserModule, PostsModule],
+    HelloModule, UsersModule, PostsModule, ProductsModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
